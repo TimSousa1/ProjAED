61324f6857840255e484567c1f5d845c35079ae0
TimSousa <martimnevessousa@gmail.com>
2023-10-23 19:35:33 +0100

    test file (unfinishe)

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,4 +1,5 @@
-extension=".singlestep"
-for folder in ../tiles/*; do for file in $folder/*.tilewalls1; do 
+extension1=".toleblists"
+extension2=".tileblasts"
+for folder in ../tiles/*; do for file in $folder/*.tilewalls; do 
     echo running on $file
     ./tileblaster $file; done; done
